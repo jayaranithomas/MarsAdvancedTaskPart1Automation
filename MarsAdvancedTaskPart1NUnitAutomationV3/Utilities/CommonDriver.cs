7e65386cd4a8d82ef08369bc4866c618981e97ae
Jayarani Thomas <jayaranithomas@gmail.com>
2024-03-22 09:07:20 +1000

    Revised: This test suite contains language, skills, aboutme, shareskills, searchskills and notification features on the Mars Portal, implented in Selenuim NUnit.

---
+++ MarsAdvancedTaskPart1NUnitAutomationV3/Utilities/CommonDriver.cs
@@ -0,0 +1,52 @@
+﻿using MarsAdvancedTaskPart1NUnitAutomation.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarsAdvancedTaskPart1NUnitAutomation.ReportClass;
+using MarsAdvancedTaskPart1NUnitAutomation.Test;
+
+
+namespace MarsAdvancedTaskPart1NUnitAutomation.Utilities
+{
+    public class CommonDriver
+    {
+
+        public static IWebDriver driver;
+
+        public static HomePage? homePageObj;
+        public static LoginWindow? loginWindowObj;
+        public static JSONReader jsonObj = new JSONReader();
+
+        [SetUp]
+        public static void BeforeTest()
+        {
+
+            homePageObj = new HomePage();
+            loginWindowObj = new LoginWindow();
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            homePageObj?.SignInAction();
+            loginWindowObj?.LoginActions();
+            Thread.Sleep(1000);
+            BaseClass.generateReport?.CreateTest();
+
+        }
+
+
+        [TearDown]
+        public static void AfterTest()
+        {
+            BaseClass.generateReport?.UpdateTest();
+            driver?.Quit();
+
+        }
+        
+
+    }
+
+}
